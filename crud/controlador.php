91ccf14ddb12eb26b7d1c2ff48565d3f0f61e161
luii2606 <valemidx@gmail.com>
2024-11-19 11:57:54 -0500

    lenguajes

---
+++ crud/controlador.php
@@ -0,0 +1,48 @@
+<?php
+require('conexion.php');
+$db = new Conexion;
+$conexion = $db->getConexion();
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$fecha_nacimiento = $_POST['fecha_nacimiento'];
+$id_genero = $_POST['id_genero'];
+$id_ciudad = $_POST['id_ciudad'];
+$lenguajes = $_POST['lenguajes'];
+
+$sql ="INSERT INTO USUARIOS (nombre_usuario, apellido_usuario, correo_usuario, fecha_nacimiento, genero, ciudad) VALUES
+(:nombre, :apellido, :correo, :fecha_nacimiento, :genero, :ciudad)";
+
+
+
+
+$stm =$conexion->prepare($sql);
+
+
+$stm ->bindParam(":nombre",$nombre);
+$stm ->bindParam(":apellido",$apellido);
+$stm ->bindParam(":correo",$correo);
+$stm ->bindParam(":fecha_nacimiento",$fecha_nacimiento);
+$stm ->bindParam(":genero",$id_genero);
+$stm ->bindParam(":ciudad",$id_ciudad);
+
+
+// $usuario= $stm-> execute();
+
+$id_usuario = $conexion-> lastInsertId();
+
+// var_dump($id_usuario);
+
+// recorer
+foreach ($lenguajes as $key => $value) {
+    $sqllenguaje ="INSERT INTO lenguaje_usuarios (id_usuario, id_lenguaje) VALUES
+    (:id_usuario, :id_lenguaje)";
+    $stmlen =$conexion->prepare($sqllenguaje);
+    $stmlen ->bindParam(":id_lenguaje",$value);
+    $stmlen ->bindParam(":id_usuario",$id_usuario);
+    
+    
+
+}
+
--- crud/index.php
+++ crud/index.php
@@ -3,27 +3,84 @@
 require('conexion.php');
 $db = new Conexion;
 $conexion = $db->getConexion();
+// var_dump($conexion);
 
 
-$sql ="SELECT * FROM ciudades";
-$bandera = $conexion->prepare($sql);
+$sqla ="SELECT * FROM ciudades";
+$bandera = $conexion->prepare($sqla);
 $bandera->execute();
 $ciudades =$bandera->fetchAll();
+
+$sqlg ="SELECT * FROM generos";
+$banderag = $conexion->prepare($sqlg);
+$banderag->execute();
+$generos =$banderag->fetchAll();
+
+$sqll ="SELECT * FROM lenguajes";
+$banderal = $conexion->prepare($sqll);
+$banderal->execute();
+$lenguajes =$banderal->fetchAll();
+
 ?>
-<form action="" method="post">
+<form action="controlador.php" method="post">
     <div>
-        <label for="ciudad_id">Ciudad</label>
-        <select name="ciudad_id" id="ciudad_id">
+        <div>
+            <label for="nombre">Nombres</label>
+            <input type="text" name="nombre">
+        </div>
+        <div>
+            <label for="apellido">Apellidos</label>
+            <input type="text" name="apellido">
+        </div>
+        <div>
+            <label for="correo">Correo</label>
+            <input type="text" name="correo">
+        </div>
+        <div>
+            <label for="nacimiento">Fecha de nacimiento</label>
+            <input type="date" name="fecha_nacimiento">
+        </div>
+        <label for="id_ciudad">Ciudad</label >
+        <select name="id_ciudad" id="id_ciudad">
             <?php
             foreach ($ciudades as $key => $value) {
                 ?>
-                <option id="<?= $value['id']?>">
-                    <?= $value['nombre']?>
+                <option name="id_ciudad" value="<?= $value['id_ciudad']?>"><?= $value['ciudad']?>
                 </option>
-                <?php
+            <?php
             }
             ?>
         </select>
+    </div>
+    <div>
+        <?php
+        foreach($generos as $key => $value){
+        ?>
+            <div>
+            <label for="genero<?= $value['id_genero'] ?>"><?= $value['genero']?>
+                <input type="radio" name="id_genero" value="<?= $value['id_genero']?>" id="genero<?= $value['id_genero']?>">
+            </label>
+            </div>
+            <?php
+            }
+            ?>
+    </div> 
+        <div>
+        <?php
+            foreach($lenguajes as $key => $value){
+        ?>
+            <div>
+                <label for="lenguajes<?= $value['id_lenguaje'] ?>"><?= $value['nombre_lenguaje']?>
+                    <input type="checkbox" name="lenguajes[]" value="<?= $value['id_lenguaje']?>" id="lenguaje<?= $value['id_lenguaje']?>">
+                </label>
+            </div>
+                <?php
+                    }
+                    ?>
+
+        </div>
+    <div>
+        <button type="submit">Enviar</button>
     </div>
 </form>
 
