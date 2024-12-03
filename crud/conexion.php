e4df11f611761c555d7e2b8dcf24c658a09c60e1
luii2606 <valemidx@gmail.com>
2024-12-03 07:44:53 -0500

    prueba

--- crud/conexion.php
+++ crud/conexion.php
@@ -3,9 +3,9 @@ class Conexion
 {
     private $conexion;
     private $host = 'localhost';
-    private $db = 'luisaa';
-    private $usuario = 'luisa_2894667';
-    private $contraseña = 'luisa';
+    private $db = 'luisa';
+    private $usuario = 'luisa_2609';
+    private $contraseña = 'luisa26';
 
     public function __construct()
     {
